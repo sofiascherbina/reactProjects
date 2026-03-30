fe4f4ac0ad471607ecc1ebe5e3986cb0d33e1fb6
Софія Щербина <160267180+sofiascherbina@users.noreply.github.com>
2026-03-30 22:28:20 +0300

    doing sth

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 // import './App.css'
+import stikers from "./stikerpak/stikers.json"
 import Mogo from "./mogo/Mogo";
 import Golden from "./golden/Golden";
 import Eats from './eats/Eats';
+import StickerList from './stikerpak/StikerList';
 function App() {
   return (
     <>
       {/* <Mogo/> */}
       {/* <Golden/> */}
-      <Eats/>
+      {/* <Eats/> */}
+      <StickerList stikers={stikers}/>
     </>
   )
 }
---
+++ src/stikerpak/StikerList.jsx
@@ -0,0 +1,44 @@
+import css from "./stikers.module.css";
+import { useState } from "react";
+export default function StickerList({stikers}){
+    const [sticker,setSticker] = useState("")
+    const showName=(label)=>{
+        setSticker(label)
+    }
+    const [selectedArr, setSelected] = useState([]);
+    const addToSel=(stiker)=>{
+            if(!selectedArr.includes(stiker)){
+                setSelected([...selectedArr, stiker])
+            }
+        return selectedArr
+    }
+    const deleteFromSel=(label)=>{
+        let filteredList = selectedArr.filter(stik => stik !== label)
+        return setSelected(filteredList) 
+    }
+    const handleToggle = (label) =>{
+         if (selectedArr.includes(label)) {
+            deleteFromSel(label);
+        } else {
+            addToSel(label); 
+        }
+    }
+    return(<>
+    <ul className={css.stickerList}>
+        {stikers.map(stiker => (<li className={css.stickerList_li} onClick={event => showName(stiker.label)}>
+            <img src={stiker.img} alt={stiker.label} width="100px" height="100px"/>
+            <button className={css.select} onClick={event => handleToggle(stiker.label)}>
+                <svg width="30px" height="30px" viewBox="5 5 90 85" xmlns="http://www.w3.org/2000/svg" >
+                    <polygon  points="50,5 61,35 95,35 67,57 78,90 50,70 22,90 33,57 5,35 39,35" fill={selectedArr.includes(stiker.label) ? "gold" : "none"} stroke="black" strokeWidth="4" />
+                </svg>
+            </button>
+        </li>))}
+    </ul>
+    <p className={css.label}>You selected : <span className={css.choice}>{sticker}</span> </p>
+    <div className={css.selectedCont}>
+        <p className={css.title}>Selected :</p>
+        <ul className={css.selectedList}>
+            {selectedArr.map(selected => (<li><p className={css.selectedEl}>{selected}</p></li>))}</ul>  
+    </div>
+    </>)
+}
