a72d082f07c1a4826b5052c223f51eea96bd66a9
Софія Щербина <160267180+sofiascherbina@users.noreply.github.com>
2026-03-31 23:43:02 +0300

    doing hw

--- src/stikerpak/StikerList.jsx
+++ src/stikerpak/StikerList.jsx
@@ -4,6 +4,7 @@ export default function StickerList({stikers}){
     const [sticker,setSticker] = useState("")
     const showName=(label)=>{
         setSticker(label)
+        counter(label);
     }
     const [selectedArr, setSelected] = useState([]);
     const addToSel=(stiker)=>{
@@ -23,15 +24,29 @@ export default function StickerList({stikers}){
             addToSel(label); 
         }
     }
+    const [number,setNumber]= useState(()=>{
+        const counters = {};
+        stikers.forEach(stik => {
+            counters[stik.label] = 0
+        });
+        return counters
+    });
+    const counter =(label)=>{
+        setNumber(prev=>({
+            ...prev,
+        [label]: prev[label]+1
+        }))
+    }
     return(<>
     <ul className={css.stickerList}>
-        {stikers.map(stiker => (<li className={css.stickerList_li} onClick={event => showName(stiker.label)}>
+        {stikers.map(stiker => (<li className={css.stickerList_li} onClick={event => {showName(stiker.label); }}>
             <img src={stiker.img} alt={stiker.label} width="100px" height="100px"/>
             <button className={css.select} onClick={event => handleToggle(stiker.label)}>
                 <svg width="30px" height="30px" viewBox="5 5 90 85" xmlns="http://www.w3.org/2000/svg" >
                     <polygon  points="50,5 61,35 95,35 67,57 78,90 50,70 22,90 33,57 5,35 39,35" fill={selectedArr.includes(stiker.label) ? "gold" : "none"} stroke="black" strokeWidth="4" />
                 </svg>
             </button>
+            <p className={css.counterText}>{number[stiker.label]}</p>
         </li>))}
     </ul>
     <p className={css.label}>You selected : <span className={css.choice}>{sticker}</span> </p>
